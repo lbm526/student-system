e37fca1a4e5e524d8f73877fee73b95a575e2680
lbm <35721483+lbm526@users.noreply.github.com>
2020-04-17 13:55:24 +0800

    更新build

---
+++ build/precache-manifest.66ddc9f90180e3079acc3177b24e1ef4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84d5bafb6268b74df0e098c8efa68f09",
+    "url": "./index.html"
+  },
+  {
+    "revision": "5ab1408c89f99319571b",
+    "url": "./static/css/main.916c3939.chunk.css"
+  },
+  {
+    "revision": "9f3986c56ae606f37bbb",
+    "url": "./static/js/2.ddf710c2.chunk.js"
+  },
+  {
+    "revision": "5ab1408c89f99319571b",
+    "url": "./static/js/main.676316c6.chunk.js"
+  },
+  {
+    "revision": "2a6465e187c41e8be799",
+    "url": "./static/js/runtime-main.1f612f7c.js"
+  }
+]);
